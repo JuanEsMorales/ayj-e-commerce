193f145896637b8e274b4ffad567d6dacf93c2ee
Juan Morales <juanchito89768@gmail.com>
2024-08-26 14:01:08 -0500

    Improving responsive and header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <header className="flex justify-between gap-3 items-center p-4 text-primaryPage font-bold sticky top-0 z-50 bg-white/70 backdrop-blur-sm w-[90%] h-24 mx-auto">
+    <header className="flex justify-between gap-3 items-center p-4 text-primaryPage font-bold sticky top-0 z-50 bg-white/70 backdrop-blur-sm w-[90%] mx-auto">
       <h1 className="text-2xl font-bold">
         <Link href="/">A<span className="text-rich">&</span>J Bisutería</Link>
       </h1>
@@ -19,12 +19,12 @@ export default function Header() {
       <ul className="gap-4 nav-icons hidden md:flex">
         <li><button><SearchIcon /></button></li>
         <li className="relative"><Link href={"/cart"}><CartIcon /><span className="absolute bottom-0 right-0 text-[10px] font-bold bg-rich text-primaryPage flex items-center justify-center w-4 h-4 rounded-[100%]">+9</span></Link></li>
-        <li><Link href={"/user"}><UserIcon /></Link></li>
+        <li className="rounded-full text-rich bg-primaryPage p-1"><Link href={"/user"}><UserIcon /></Link></li>
       </ul>
       <label htmlFor="hamburger-menu" className="hamburger-menu flex flex-col md:hidden">
         <input type="checkbox" name="hamburger-menu" id="hamburger-menu" />
       </label>
-      <nav className="nav-responsive fixed w-full top-0 left-0 gap-4 nav-links bg-white/90 backdrop-blur-xl shadow-white/90 shadow-xl md:hidden">
+      <nav className="nav-responsive fixed w-full top-0 left-0 gap-4 nav-links bg-white/90 backdrop-blur-xl md:hidden pb-4">
         <ul className="flex flex-col justify-center items-center gap-4 text-xl mt-4">
           <li><Link href={"/"}>Inicio</Link></li>
           <li><Link href={"/collections"}>Colecciones</Link></li>
@@ -34,7 +34,7 @@ export default function Header() {
         <ul className="flex justify-center gap-4 mt-4">
           <li><button><SearchIcon /></button></li>
           <li className="relative"><Link href={"/cart"}><CartIcon /><span className="absolute bottom-0 right-0 text-[10px] font-bold bg-rich text-primaryPage flex items-center justify-center w-4 h-4 rounded-[100%]">+9</span></Link></li>
-          <li><Link href={"/user"}><UserIcon /></Link></li>
+          <li className="rounded-full text-rich bg-primaryPage p-1"><Link href={"/user"}><UserIcon /></Link></li>
         </ul>
       </nav>
     </header>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default function Hero() {
   return (
-    <section className="flex flex-col-reverse md:flex-row justify-center gap-10 w-full items-center p-4 text-rich">
-      <div className="flex flex-col gap-3 items-center md:items-start max-w-[630px]">
+    <section className="flex flex-col-reverse md:flex-row justify-center gap-10 w-full relative items-center p-4 text-rich">
+      <div className="flex flex-col gap-3 items-center absolute md:static top-[calc(50%-140px)] md:items-start max-w-[630px] z-10">
         <h1 className="text-3xl md:text-4xl text-center md:text-left font-bold">
           Luce tu <span className="text-primary text-wrap">esencia</span> con nuestra bisutería artesanal.
         </h1>
@@ -12,7 +12,7 @@ export default function Hero() {
           Ir a la tienda
         </Link>
       </div>
-      <div className="flex flex-col gap-3 items-start">
+      <div className="flex opacity-90 md:opacity-100 flex-col gap-3 items-start">
         <Image src={"https://m.media-amazon.com/images/I/61P1y4UO9UL.jpg"} alt="imagen" width={500} height={500} />
       </div>
     </section>
